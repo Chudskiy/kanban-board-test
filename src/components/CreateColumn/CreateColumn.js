de3f9779d24390eccc1a1b06b445a22398563803
Chudskii-Semyon <chudskii.semyon@gmail.com>
2019-08-11 14:41:08 +0300

    add adding column on enter feature

--- src/components/CreateColumn/CreateColumn.js
+++ src/components/CreateColumn/CreateColumn.js
@@ -6,7 +6,7 @@ import {add_column} from "../../store/actions/columns";
 const CreateColumn = (props) => {
     const [inputText, setInputText] = useState('');
 
-    const boardId = useSelector(state => state.boards[0].id);
+    const boardId = useSelector(({boards}) => boards[0].id);
 
     const dispatch = useDispatch();
 
@@ -14,32 +14,39 @@ const CreateColumn = (props) => {
         setInputText(target.value)
     };
 
+    const onKeyDownHandler = ({key}) => {
+        if (key === 'Enter') {
+            addColumnHandler();
+            setInputText('')
+        }
+    };
+
     const addColumnHandler = () => {
-        const payload = {
+        dispatch(add_column({
             id: uuidv4(),
             title: inputText,
             boardId: boardId
-        };
-
-        dispatch(add_column(payload))
+        }))
     };
 
     return (
         <div
             className="flex justify-between items-center p-3 lg:w-64 bg-gray-200 border border-gray-500"
-            style={{
-                minWidth: '250px'
-            }}
+            style={{minWidth: '250px'}}
         >
             <input
-                onChange={inputChangeHandler} value={inputText}
+                onChange={inputChangeHandler}
+                onKeyDown={onKeyDownHandler}
+                value={inputText}
                 className="p-2 w-full bg-gray-100 border border-gray-500 rounded"
                 type="text"
                 placeholder="Add new column"
             />
 
-            <button onClick={addColumnHandler}
-                    className="p-2 bg-gray-300 hover:bg-gray-500 rounded">Add
+            <button
+                onClick={addColumnHandler}
+                className="p-2 bg-gray-300 hover:bg-gray-500 rounded">
+                Add
             </button>
         </div>
     );
