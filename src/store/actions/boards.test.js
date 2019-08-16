8adfc66a235b7d4dd5bdf4b67b9272ca79dcbb5c
BladeRunnerBlade <manunited150891@gmail.com>
2019-08-16 11:59:12 +0300

    refactor boards actions tests

--- src/store/actions/boards.test.js
+++ src/store/actions/boards.test.js
@@ -4,52 +4,29 @@ import {ADD_BOARD, ADD_COLUMN_TO_BOARD, REMOVE_COLUMN_FROM_BOARD} from "./types"
 
 describe('boards actions', () => {
     it('should create an action to add board', () => {
-        const id = uuidv4();
-
-        const payload = {
-            id,
-            title: "board title"
-        };
-
         const expectedAction = {
             type: ADD_BOARD,
-            payload
+            payload: {}
         };
 
-        expect(add_board(payload)).toEqual(expectedAction)
+        expect(add_board({})).toEqual(expectedAction)
     });
 
     it('should create an action to add column to board', () => {
-        const boardId = uuidv4();
-        const columnId = uuidv4();
-
-        const payload = {
-            boardId,
-            columnId
-        };
-
         const expectedAction = {
             type: ADD_COLUMN_TO_BOARD,
-            payload
+            payload: {}
         };
 
-        expect(add_column_to_board(payload)).toEqual(expectedAction)
+        expect(add_column_to_board({})).toEqual(expectedAction)
     });
 
     it('should create an action to remove column from board', () => {
-        const boardId = uuidv4();
-        const columnId = uuidv4();
-
-        const payload = {
-            boardId,
-            columnId
-        };
-
         const expectedAction = {
             type: REMOVE_COLUMN_FROM_BOARD,
-            payload
+            payload: {}
         };
 
-        expect(remove_column_from_board(payload)).toEqual(expectedAction)
+        expect(remove_column_from_board({})).toEqual(expectedAction)
     });
 });
